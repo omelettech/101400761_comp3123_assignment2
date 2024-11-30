a3610f51b4113a08db12b1f092e0895a8431a1ac
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-30 16:39:37 -0500

    Added links and empty button functions

--- src/screens/Employee/Employee.tsx
+++ src/screens/Employee/Employee.tsx
@@ -47,33 +47,55 @@ const Employee = ({}: EmployeeProps) => {
         navigate("/");
     };
 
+    const addEmployee = () => {
+
+    };
+    const findEmployee = () => {
+        //TODO: Place find emp logic
+        console.log("hello emp")
+    };
     return (
         <div>
+            <button onClick={handleLogout}>Logout</button>
+
             <h1>Employee Dashboard</h1>
             <p>Welcome to the employee dashboard!</p>
-            <table style={{width: "100%", borderCollapse: "collapse"}}>
+            <button onClick={addEmployee}>Add Employee +</button>
+            <table style={{width: "100%", borderCollapse: "collapse", border:1,borderWidth:1,borderStyle:"solid"}}>
                 <thead>
                 <tr>
-                    <th>ID</th>
+                    {/*<th>ID</th>*/}
                     <th>Name</th>
                     <th>Position</th>
                     <th>Email</th>
                     <th>Salary</th>
                     <th>date_of_joining</th>
                     <th>Department</th>
+                    <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 {filteredEmployeeList.length > 0 ? (
                     filteredEmployeeList.map((employee) => (
-                        <tr key={employee._id}>
-                            <td>{employee._id}</td>
+                        <tr key={employee._id}
+                            onClick={findEmployee}
+                            style={{
+                                cursor:"pointer",
+                                fontSize:18,
+                                textDecoration:"underline",
+                                color:"blue",
+                        }}
+                        >
+                            {/*<td>{employee._id}</td>*/}
                             <td>{`${employee.first_name} ${employee.last_name}`}</td>
                             <td>{employee.position}</td>
                             <td>{employee.email}</td>
                             <td>${employee.salary.toLocaleString()}</td>
                             <td>{employee.date_of_joining.slice(0,10)}</td>
                             <td>{employee.department}</td>
+                            <td>
+
+                            </td>
 
                         </tr>
                     ))
@@ -86,7 +108,6 @@ const Employee = ({}: EmployeeProps) => {
                 )}
                 </tbody>
             </table>
-            <button onClick={handleLogout}>Logout</button>
         </div>
     );
 };
